b878111e358bf6e9b4631ff52b41959c570a32c8
ADITYA GUPTA <agps.shutter@gmail.com>
2020-09-24 19:38:58 +0530

    Updates

---
+++ precache-manifest.1bf8db3d54ce3aa5245188c691c2f9e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "03052b7286cb107f5ba9",
+    "url": "/Robo-friends/static/css/main.76619677.chunk.css"
+  },
+  {
+    "revision": "03052b7286cb107f5ba9",
+    "url": "/Robo-friends/static/js/main.68da688c.chunk.js"
+  },
+  {
+    "revision": "8aa4eed7b9ae81d2c96d",
+    "url": "/Robo-friends/static/js/runtime~main.bee2c3ee.js"
+  },
+  {
+    "revision": "332f9416a1073340914e",
+    "url": "/Robo-friends/static/css/2.17856a3f.chunk.css"
+  },
+  {
+    "revision": "332f9416a1073340914e",
+    "url": "/Robo-friends/static/js/2.7292f385.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robo-friends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "dcd1f9a9dd6b1ba8a403857f8cd79fac",
+    "url": "/Robo-friends/index.html"
+  }
+];
